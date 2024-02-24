0372ea7d72873c421fca93d2baaf9931a3f44d36
Nunzio <chiefa@gmail.com>
2024-02-24 23:10:02 +0100

    create chart component,  setup get doc from db, rendering test data in view

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,17 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonCol, IonContent, IonFooter, IonGrid, IonHeader, IonPage, IonRouterLink, IonRow, IonTitle, IonToolbar } from '@ionic/react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+      <Header />
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
         <ExploreContainer />
       </IonContent>
+      <Footer />
     </IonPage>
   );
 };
